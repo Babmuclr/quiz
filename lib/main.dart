48034902f0aa889c7c4746eb5454a8015bfa2800
Babmuclr <it.onepiece.balsreal@gmail.com>
2021-06-21 14:55:26 +0900

    12 questions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
+import 'quizbank.dart';
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +27,26 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+
+  Icon createIcon (bool isCollect) {
+    var icon;
+    if (isCollect) {
+      icon = Icon(
+        Icons.check,
+        color: Colors.green,
+      );
+    }else {
+      icon = Icon(
+        Icons.close,
+        color: Colors.red,
+      );
+    }
+    return icon;
+  }
+
+  List<Icon> scoreKeeper = [];
+  Quizbank quizbank = Quizbank();
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +59,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizbank.getQuizText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -50,9 +72,10 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.green,
+              ),
               child: Text(
                 'True',
                 style: TextStyle(
@@ -61,7 +84,12 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                setState(() {
+                  scoreKeeper.add(
+                    createIcon(true == quizbank.getQuizAnswer()),
+                  );
+                  quizbank.nextQuestion();
+                });
               },
             ),
           ),
@@ -69,8 +97,10 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.red,
+              ),
               child: Text(
                 'False',
                 style: TextStyle(
@@ -79,12 +109,20 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                setState(() {
+                  scoreKeeper.add(
+                    createIcon(false == quizbank.getQuizAnswer()),
+                  );
+                  quizbank.nextQuestion();
+                });
               },
             ),
           ),
         ),
         //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
